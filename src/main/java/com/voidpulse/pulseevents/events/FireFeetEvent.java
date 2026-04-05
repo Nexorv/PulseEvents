f6696873ea92f8547d048d5aefbd287d6023be14
Wilczek015 <gabrielmalagowski17@gmail.com>
2026-04-05 14:34:03 +0200

    Fix of FireFeetEvent

--- src/main/java/com/voidpulse/pulseevents/events/FireFeetEvent.java
+++ src/main/java/com/voidpulse/pulseevents/events/FireFeetEvent.java
@@ -1,8 +1,7 @@
 package com.voidpulse.pulseevents.events;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
@@ -23,14 +22,23 @@ public class FireFeetEvent implements PulseEvent {
 
     @Override
     public void start() {
+
         task = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+
             for (Player p : Bukkit.getOnlinePlayers()) {
-                Location loc = p.getLocation().clone().subtract(0, 1, 0);
-                if (loc.getBlock().getType() == Material.AIR) {
-                    loc.getBlock().setType(Material.FIRE);
-                }
+
+                if (!p.isOnGround()) continue;
+
+                p.getWorld().spawnParticle(
+                        Particle.FLAME,
+                        p.getLocation().add(0, 0.1, 0),
+                        8,
+                        0.2, 0.0, 0.2,
+                        0.01
+                );
             }
-        }, 0L, 20L);
+
+        }, 0L, 2L);
     }
 
     @Override
@@ -40,6 +48,6 @@ public class FireFeetEvent implements PulseEvent {
 
     @Override
     public int getDuration() {
-        return 35;
+        return 30;
     }
 }
